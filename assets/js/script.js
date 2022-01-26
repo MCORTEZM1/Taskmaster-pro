004e77b0d6e6b6e3e3a1048984aa003c7264da49
Michael Cortez-Mejia <you@example.com>
2022-01-25 23:45:36 -0800

    drag and drop features added for desktop and mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,54 @@
 var tasks = {};
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false, 
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over", this);
+  },
+  out: function(event){
+    console.log("out", this);
+  },
+  update: function(event){
+    var tempArr = [];
+
+    $(this).children().each(function(){
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim()
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+  // trim down list's ID to match object property
+  var arrName =$(this)
+    .attr("id")
+    .replace("list-", "")
+  
+  // update array on tasks object and save 
+  tasks[arrName] = tempArr;
+  saveTasks();
+  }  
+});
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -178,6 +227,22 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+
+// delete droppable 
+$("#trash").droppable({
+  accept:".card .list-group-item",
+  tolerance:"touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
 });
 
 // load tasks for the first time
